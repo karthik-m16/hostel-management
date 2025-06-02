3ebc07ff16e5eeec0dec13ed0327294c6142d774
karthik-m16 <karthik.thanos@example.com>
2025-06-02 16:48:52 +0530

    Updated Flask imports and removed deprecated Werkzeug functionality

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
-DATABASE_URI = "postgresql://username:password@your-cloud-sql-ip/dbname"
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = DATABASE_URI
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
+    SECRET_KEY = os.getenv("SECRET_KEY")
